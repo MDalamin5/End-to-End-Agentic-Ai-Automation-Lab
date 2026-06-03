331c0838b06926316e9469965d9d82cfbd107805
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-03 21:12:56 +0600

    agent config schemeas creatinos

--- 13-e2e-Project-Deep-Agents-With-LangGraph/notebooks/schemas.py
+++ 13-e2e-Project-Deep-Agents-With-LangGraph/notebooks/schemas.py
@@ -1,1 +1,12 @@
 from pydantic import BaseModel
+
+class AgentConfig(BaseModel):
+    name: str
+    description: str
+    capabilities: list[str]
+    constraints: list[str]  
+
+class TaskConfig(BaseModel):
+    task_name: str
+    task_description: str
+    agents: list[AgentConfig]
